a2b56e2b7f874e5478f16a22261299bc68a4be97
Bruno-Felix <balvesfelix@gmail.com>
2024-09-27 12:56:40 -0300

    Redimensionando gifs | Retirando codigo

--- main.py
+++ main.py
@@ -37,30 +37,22 @@ async def on_message(message):
         return
 
     if 'quack' == (str(message.content).lower()):
+        gif_number = randint(1, 2)
+
         await message.channel.send('QUACK Yeonji mencionada!!')
-        await message.channel.send(file=discord.File(f'static/quack_{1}.gif'))
+        await message.channel.send(file=discord.File(f'static/quack_{gif_number}.gif'))
 
-    if 'maluca' in (str(message.content).lower()):
-        try:
-            await message.add_reaction('<:tohrn1S4:1104808399343996938>')
-        except Exception:
-            pass
-        if 'maluca' == (str(message.content).lower()):
-            gif_number = randint(1, 5)
+    if 'maluca' == (str(message.content).lower()):
+        gif_number = randint(1, 5)
 
-            await message.channel.send('MALUCA mencionada!!')
-            await message.channel.send(file=discord.File(f'static/chaeyeon_{gif_number}.gif'))
+        await message.channel.send('MALUCA mencionada!!')
+        await message.channel.send(file=discord.File(f'static/chaeyeon_{gif_number}.gif'))
 
-    if 'medica' in (str(message.content).lower()) or 'médica' in (str(message.content).lower()):
-        try:
-            await message.add_reaction('<:tohrn1OK:1158268768779239544>')
-        except Exception:
-            pass
-        if 'medica' == (str(message.content).lower()) or 'médica' == (str(message.content).lower()):
-            gif_number = randint(1, 5)
+    if 'medica' == (str(message.content).lower()) or 'médica' == (str(message.content).lower()):
+        gif_number = randint(1, 5)
 
-            await message.channel.send('MEDICA mencionada!!')
-            await message.channel.send(file=discord.File(f'static/medica{gif_number}.gif'))
+        await message.channel.send('MEDICA mencionada!!')
+        await message.channel.send(file=discord.File(f'static/medica{gif_number}.gif'))
 
     if 'kaede' in (str(message.content).lower()) or 'kaebeça' in (str(message.content).lower()):
         await message.add_reaction('<:kaebeca:1234954376158773308>')
@@ -73,8 +65,11 @@ async def on_message(message):
 
     if 'fifinha' in (str(message.content).lower()):
         await message.add_reaction('⚽')
+
         if "?" in (str(message.content).lower()):
             await message.add_reaction('❓')
+
+# --------------
 
     if '!fifa' == (str(message.content).lower()):
         user_ids = [701955314161025086, 321389614940028929, 1028533279802011688, 406686196581007361,
@@ -85,7 +80,7 @@ async def on_message(message):
         mentions = " ".join([f"<@{user_id}>" for user_id in user_ids])
 
         await message.channel.send(f'{mentions}')
-        await message.channel.send(file=discord.File('static/fifa1.gif'))
+        await message.channel.send(file=discord.File('static/fifa.gif'))
 
     if "!live" == (str(message.content).lower()):
         await message.channel.send(file=discord.File('static/tohrjob.gif'))
