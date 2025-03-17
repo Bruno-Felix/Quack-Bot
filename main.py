e3754e82c8b29cea0020f105e25ce5f28983eef2
Bruno-Felix <balvesfelix@gmail.com>
2025-03-17 05:10:56 -0300

    Automatizando envio de lancamentos diarios no canal da quack

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import os
 import discord
+import asyncio
+import schedule
 from os.path import join, dirname
 from dotenv import load_dotenv
 from discord.ext import commands
 
-from src.music import calendar
+from src.commands.music import Music
 
 dotenv_path = join(dirname(__file__), '../.env')
 load_dotenv(dotenv_path)
@@ -24,6 +26,17 @@ async def load_commands():
             await bot.load_extension(f'src.commands.{arquivo[:-3]}')
 
 
+async def schedule_loop():
+    print('schedule sincronizado')
+    music_cog = bot.get_cog("Music")
+
+    schedule.every().day.at("08:00").do(lambda: asyncio.create_task(music_cog.hoje_diario()))
+
+    while True:
+        schedule.run_pending()
+        await asyncio.sleep(60)
+
+
 @bot.event
 async def on_ready():
     try:
@@ -33,6 +46,8 @@ async def on_ready():
 
         print(f'\nQuack Bot pronta!! Conectado como {bot.user}')
         print(f'{len(synced)} comandos sincronizados')
+
+        await bot.loop.create_task(schedule_loop())
     except Exception as error:
         print(error)
 
--- src/commands/music.py
+++ src/commands/music.py
@@ -1,19 +1,46 @@
+import os
 import discord
 from discord.ext import commands
 from discord import app_commands
 from datetime import date, datetime
 from zoneinfo import ZoneInfo
+from dotenv import load_dotenv
 
 from static.triples_colors import get_sort_triples_color
 from src.music import calendar
 
 brasilia_tz = ZoneInfo("America/Sao_Paulo")
 
+dotenv_path = os.path.join(os.path.dirname(__file__), '../../.env')
+load_dotenv(dotenv_path)
+
+QUACK_BOT_CHANNEL_ID = os.getenv('QUACK_BOT_CHANNEL_ID')
+
+
 class Music(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         super().__init__()
 
+    async def hoje_diario(self):
+        guild = self.bot.guilds[0]
+        channel = guild.get_channel(int(QUACK_BOT_CHANNEL_ID))
+
+        embed = discord.Embed(
+            title='Lançamentos do dia!!',
+            description='Fique de olho em todos os lançamentos de hoje\n',
+            color=get_sort_triples_color()
+        )
+
+        search_date = str(datetime.now(brasilia_tz).date())
+
+        results = await calendar.get_daily_kpop_calendar(search_date)
+
+        for result in results:
+            embed.add_field(name="\u200b", value=result, inline=False)
+
+        if channel:
+            await channel.send(embed=embed)
 
     @app_commands.command(description='Veja os lançamentos de kpop do dia')
     async def hoje(self, interaction: discord.Interaction):
