95457965b6dbe9714b1b23ac7a5872d3f96552a0
Herik Cosmo <herikcosmo.m@gmail.com>
2025-03-30 10:40:33 -0300

    Ajuste na função get_users_by_reactions

--- src/mentions.py
+++ src/mentions.py
@@ -5,7 +5,7 @@ dotenv_path = os.path.join(os.path.dirname(__file__), '../.env')
 load_dotenv(dotenv_path)
 
 async def get_users_by_reaction(self, emoji: str):
-    channel_id = int(os.getenv('REACTIONS_CHANNEL_ID'))
+    channel_id = os.getenv('REACTIONS_CHANNEL_ID')
     message_id = os.getenv('REACTIONS_MESSAGE_ID')
 
     channel = await self.bot.fetch_channel(int(channel_id))
